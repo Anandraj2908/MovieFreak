ca1c6c9ebe76cc1625fef66bf221c842cc6f76eb
Anand Raj <anandraj.solutions@gmail.com>
2023-12-05 23:56:55 +0530

    configured basic project structure and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from 'react'
 import  fetchDataFromApi  from './utils/api'
+import { useSelector, useDispatch } from 'react-redux'
+
+//import components
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import PageNotFound from "./pages/404/PageNotFound"
+import Details from "./pages/details/Details"
+import Explore from "./pages/explore/Explore"
+import Home from "./pages/home/Home"
+import SearchResult from './pages/searchResult/SearchResult'
 
 function App() {
-  let MyObj=null;
+  const dispatch = useDispatch();
+
 
   const apiTest = () => {
     fetchDataFromApi('/movie/popular').then(
---
+++ src/pages/404/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.scss"
+
+const PageNotFound = () => {
+  return (
+    <div>
+      PageNot found
+    </div>
+  )
+}
+
+export default PageNotFound
