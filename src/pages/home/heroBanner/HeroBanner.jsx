dc01caeb35c0fa61c804573afcf34b35f1d3647c
Anand Raj <anandraj.solutions@gmail.com>
2023-12-06 22:26:15 +0530

    img component added

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux';
 import "./style.scss"
 
 import useFetch from '../../../hooks/useFetch';
-
+import Img from '../../../components/lazyLoadImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 
 const HeroBanner = () => {
 
