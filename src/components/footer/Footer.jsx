e6ec600da010764412a0655f590a8d7d6611f173
Anand Raj <anandraj.solutions@gmail.com>
2023-12-07 02:15:03 +0530

    footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React from "react";
+import {
+    FaGithub,
+    FaLinkedin,
+} from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { FaHashnode,FaMedium } from "react-icons/fa6";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./style.scss";
 
 const Footer = () => {
-  return (
-    <footer>Footer</footer>
-  )
-}
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                
+                <h2 className="infoText">
+                    A React project by Anand Raj
+                </h2>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="socialIcons">
+                    
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                    <span className="icon">
+                        <FaGithub />
+                    </span>
+                    <span className="icon">
+                        <FaMedium />
+                    </span>
+                    <span className="icon">
+                        <FaHashnode />
+                    </span>
+                    <span className="icon">
+                        <SiLeetcode />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
