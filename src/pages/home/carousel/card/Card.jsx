0827f2854a1067dde4b82652a9f48bc8b068878c
Anand Raj <anandraj.solutions@gmail.com>
2023-12-10 22:13:00 +0530

    trial

--- src/pages/home/carousel/card/Card.jsx
+++ src/pages/home/carousel/card/Card.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import "./style.scss"
 const Card = ({imageUrl,item}) => {
+  console.log(item)
   return (
-    <div className="rounded-lg shadow bg-gray-800 border-gray-700 mx-3 my-5 min-w-[16%]">
-    <img className="rounded-t-lg" src={imageUrl} alt="" />
-    <div className="p-5">
-        
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.title}</p>
-        
+    <div className="myCard rounded-lg shadow bg-gray-800 border-gray-700 mx-3 my-5 ">
+      <img className="rounded-t-lg" src={imageUrl} alt="" />
+      <p className='title mx-3 my-5'>{item.title}</p>
     </div>
-</div>
   )
 }
 
